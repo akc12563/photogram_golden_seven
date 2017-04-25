167b3edb5d043bd158d0a674d2664f6078128afd
Amy Collins <amy.collins@user.noreply.github.com>
2017-04-25 09:40:23 -0500

    got flexible part working

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,10 @@
+class PhotosController <ApplicationController
+  def show
+    #the params hash looks like this : {"the_id_number"=>"1"}
+    p= Photo.find(params["the_id_number"])
+    @the_caption = p.caption
+    @the_image_url = p.source
+    @created_at =p.created_at
+render("photos/show.html.erb")
+  end
+end
